7eddc59b4d02ae7a84a0d7e5fcf30640dc3045fa
sMC-Lean <125336469+sMC-Lean@users.noreply.github.com>
2024-05-22 23:43:43 +1000

    header, hero and tabbed component for process complete
    sticky navbar data and behaviour complete, hero banner and buttons with CTA complete, tabbed component data and behaviour complete, added data for breeds slider, behaviour incomplete

--- script.js
+++ script.js
@@ -1,3 +1,71 @@
 "use strict";
 
-console.log("hello world");
+// sticky nav bar;
+const nav = document.querySelector(".nav-bar");
+const header = document.querySelector(".header");
+// const navHeight = nav.getBoundingClientRect().height;
+const processSection = document.querySelector(".process");
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${nav.getBoundingClientRect().height}px`,
+});
+
+headerObserver.observe(header);
+
+// navbar hover animation for links;
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav-link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav-bar").querySelectorAll(".nav-link");
+    const button = link.closest(".nav-bar").querySelector(".contact");
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    // button.style.opacity = this;
+  }
+};
+
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
+
+document.querySelector(".nav-links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("nav-link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+const tabs = document.querySelectorAll(".process-tab");
+const tabsContainer = document.querySelector(".process-tab-container");
+const tabsContent = document.querySelectorAll(".process-content");
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".process-tab");
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach((t) => t.classList.remove("process-tab-active"));
+  tabsContent.forEach((c) => c.classList.remove("process-content-active"));
+
+  // Activate tab
+  clicked.classList.add("process-tab-active");
+
+  // Activate content area
+  document
+    .querySelector(`.process-content-${clicked.dataset.tab}`)
+    .classList.add("process-content-active");
+});
